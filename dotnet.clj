fb982f3a723c3ff46200b32b9af2e46f4e3f6060
loicb <me@loicb.dev>
2026-06-04 13:01:52 +0800

    chore(clr): Migrate to MAGIC 0.4.0

--- dotnet.clj
+++ dotnet.clj
@@ -7,44 +7,20 @@
   This namespace provides convenient functions to:
   - compile the prod namespaces to .net assemblies
   - run the tests in the CLR"
-  (:require [clojure.test :refer [run-all-tests]]
-            [magic.flags :as mflags]))
+  (:require [nostrand.tasks :as tasks]))
 
-(def prod-namespaces
-  '[robertluo.fun-map.wrapper
-    robertluo.fun-map.helper
-    robertluo.fun-map.core
-    robertluo.fun-map])
-
-(def test-namespaces
-  '[robertluo.fun-map-test
-    robertluo.fun-map.core-test
-    robertluo.fun-map.wrapper-test])
+(def jvm-only
+  "JVM-only namespaces that must not load on the CLR."
+  '[robertluo.fun-map.util])
 
 (defn build
   "Compiles the project to dlls.
   nos dotnet/build"
   []
-  (binding [*compile-path*                  "build"
-            *unchecked-math*                *warn-on-reflection*
-            mflags/*strongly-typed-invokes* true
-            mflags/*direct-linking*         true
-            mflags/*elide-meta*             false]
-    (println "Compile into DLL To : " *compile-path*)
-    (doseq [ns prod-namespaces]
-      (println (str "Compiling " ns))
-      (compile ns))))
+  (tasks/compile-project :exclude jvm-only :clean? true))
 
 (defn run-tests
   "Run all the tests on the CLR.
   nos dotnet/run-tests"
   []
-  (binding [*unchecked-math*                *warn-on-reflection*
-            mflags/*strongly-typed-invokes* true
-            mflags/*direct-linking*         true
-            mflags/*elide-meta*             false]
-    (doseq [ns (concat prod-namespaces test-namespaces)]
-      (require ns))
-    (let [{:keys [fail error]} (run-all-tests)]
-      (when (or (pos? fail) (pos? error))
-        (Environment/Exit 1)))))
+  (tasks/run-clojure-tests :aliases [:test] :exclude jvm-only))
